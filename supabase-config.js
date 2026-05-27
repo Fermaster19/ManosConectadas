b471e74d8594fd99a874f0c6e23d355aa4cee2e6
Fermaster19 <claudiovdr20@gmail.com>
2026-05-27 19:26:56 -0300

    first commit

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+// Reemplaza estos valores con los de tu proyecto Supabase.
+window.SUPABASE_URL = "https://icfjdcnelprodsjioicm.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_rWUvezpEaIvObe_lfqN5UQ_Uk8N5sdm";
